233477ae32b7c03570d40ffb2a8f3d1ef3ae9edb
H129539 <Vijayaprabha.Brahma@Honeywell.com>
2018-08-10 01:00:12 +0530

    Change to serveo redirected hostname

--- authentication.js
+++ authentication.js
@@ -5,7 +5,7 @@ var OnshapeStrategy = require('passport-onshape').Strategy;
 
 var oauthClientId;
 var oauthClientSecret;
-var callbackUrl = "https://still-springs-45157.herokuapp.com/oAuthRedirect";
+var callbackUrl = "https://solutio.serveo.net/oAuthRedirect";
 var oauthUrl = 'https://oauth.onshape.com';
 var apiUrl = 'https://cad.onshape.com';
 
